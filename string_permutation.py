018066357b6b7d3c0ef5e1e4358f81af67e7b2f3
boldPro <probold777@gmail.com>
2023-07-12 14:32:43 -0400

    permutation str

--- string_permutation.py
+++ string_permutation.py
@@ -3,37 +3,48 @@ def next_permutation(s):
     arr = list(s)
     lenth = len(arr)
     i = lenth - 2
+
+
+    # def rev(arr, start, end):
+    #     while start < end:
+    #         temp_memo = arr[start]
+    #         arr[start] = arr[end]
+    #         arr[end] = temp_memo
+    #     start += 1
+    #     end -= 1
+    #     print(f"in fu{arr}")
+    #     return arr
+
      
     # Find the largest index i such that arr[i] < arr[i+1]
     while i >= 0 and arr[i] >= arr[i+1]:
         i -= 1
-    
      # If no such index exists, return "No next Permutation"
     if i < 0:
         return "No next Permutation possible"
+    
+
     j = lenth - 1
-     
      # Find the largest index j such that arr[i] < arr[j]
     while j >= 0 and arr[j] <= arr[i]:
         j -= 1
     # Swap arr[i] and arr[j]
     arr[i], arr[j] = arr[j], arr[i]
     # Reverse the sublist arr[start:end+1]
-    rev(arr, i+1, lenth-1)
+    # rev(arr, i+1, lenth-1)
     return ''.join(arr)
  
  # Function to reverse the array
-def rev(arr: list, start: int, end: int) -> None:
-    while start < end:
-        swap(arr, start, end)
-        start += 1
-        end -= 1
- # Function to swap two numbers
-def swap(arr: list, i: int, j: int) -> None:
-    temp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = temp
+
  
 
-s = "abac"
+s = "aabc"
 print(next_permutation(s))
+
+
+
+
+# if s[-2]>s[-1]:
+#     print("Pre last one is more")
+# else:
+#     print("Pre last one is less")
