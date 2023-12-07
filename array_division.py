50613132eb59f6858ad694e81cdadde63b4edb3e
boldPro <probold777@gmail.com>
2023-12-07 11:05:45 -0500

    arr

--- array_division.py
+++ array_division.py
@@ -6,18 +6,6 @@ def birthday(s, d, m):
     for i in range(0,(len(s))):
         if sum(s[i:i+m]) == d:
                 ron_squares += 1
-
-        # print(f'{s[i]} i')
-        # print(f'{d} d')
-        # print(s[i] == d)
-        # if s[i] == d:
-        #     sumary= s[i]
-        # else:
-        #     for j in range(i+1,m):  
-        #         sumary = s[i]+s[j]
-        #         # print(sum)
-        # if sumary == d:
-        #     ron_squares+=1
     
     return ron_squares
 
